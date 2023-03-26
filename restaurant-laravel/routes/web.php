50ee27cd33563b5d7fe837882c01f54e118f12b6
Supakorn Charoen <supakorncharoen@gmail.com>
2023-03-26 17:13:45 +0700

    [02-laravel-front-end] - Setuo Admin Layout

--- restaurant-laravel/routes/web.php
+++ restaurant-laravel/routes/web.php
@@ -16,6 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+Route::get('/admin', function () {
+    return view('admin/dashboard');
+});
+
+
 Route::get('/menu', function () {
     return view('menu/index');
 });
