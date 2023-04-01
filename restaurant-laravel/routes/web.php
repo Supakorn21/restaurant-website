c0d3830a1c1425f148b922b0c78ef52df7caf163
Supakorn Charoen <supakorncharoen@gmail.com>
2023-04-01 15:59:23 +0700

    [02-laravel-front-end] - Create Controllers for Static pages and admin sections

--- restaurant-laravel/routes/web.php
+++ restaurant-laravel/routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\admin\FoodCategoriesController;
+use App\Http\Controllers\admin\AdminController;
+use App\Http\Controllers\StaticPagesController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,43 +14,28 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+// Homepage
+Route::get('/', [StaticPagesController::class, 'home']);
 
-Route::get('/', function () {
-    return view('home');
-});
-Route::get('/admin', function () {
-    return view('admin/dashboard');
-});
-Route::get('/admin/food-categories', function () {
-    return view('admin/food-categories/all');
-});
-Route::get('/admin/food-categories/create', function () {
-    return view('admin/food-categories/create');
-});
-Route::get('/admin/food-categories/{id}/edit', function () {
-    return view('admin/food-categories/edit');
-});
+// Static Pages
+Route::get('/menu', [StaticPagesController::class, 'menu']);
+Route::get('/menu/{slug}', [StaticPagesController::class, 'singleMenu']);
+Route::get('/about', [StaticPagesController::class, 'about']);
+Route::get('/waitlist', [StaticPagesController::class, 'waitlist']);
+Route::get('/offers', [StaticPagesController::class, 'offers']);
+Route::get('/contact', [StaticPagesController::class, 'contact']);
+
+// Admin Dashboard
+Route::get('/admin', [AdminController::class, 'dashboard']);
+// Admin Food Categories
+Route::get('/admin/food-categories', [FoodCategoriesController::class, 'index']);
+Route::get('/admin/food-categories/create', [FoodCategoriesController::class, 'create']);
+Route::get('/admin/food-categories/{id}/edit', [FoodCategoriesController::class, 'edit']);
+
+// Admin Authentication
 Route::get('/admin/register', function () {
     return view('admin/register');
 });
 Route::get('/admin/login', function () {
     return view('admin/login');
-});
-
-
-Route::get('/menu', function () {
-    return view('menu/index');
-});
-Route::get('/menu/{slug}', function () {
-    return view('menu/single-menu');
-});
-Route::get('/about', function () {
-    return view('pages/about');
-});
-Route::get('/waitlist', function () {
-    return view('pages/waitlist');
-});
-
-Route::get('/offers', function () {
-    return view('pages/offers');
 });
