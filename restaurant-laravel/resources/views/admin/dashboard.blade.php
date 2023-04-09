7e802ace7e5789a77afe3b68d25c1f3eb1263a16
Supakorn Charoen <supakorncharoen@gmail.com>
2023-04-09 16:01:36 +0700

    [03-laravel-back-end] - Setting Up to start using chart in JS page and dashboard page

--- restaurant-laravel/resources/views/admin/dashboard.blade.php
+++ restaurant-laravel/resources/views/admin/dashboard.blade.php
@@ -25,6 +25,7 @@
                         </div>
                     </div>
                 </div>
+                
             </div>
             <!-- ============================================================== -->
             <!-- end pageheader  -->
@@ -43,7 +44,7 @@
                                     <span><i class="fa fa-fw fa-arrow-up"></i></span><span>5.86%</span>
                                 </div>
                             </div>
-                            <div id="sparkline-revenue"></div>
+                            {{-- <div id="sparkline-revenue"></div> --}}
                         </div>
                     </div>
                     <div class="col-xl-3 col-lg-6 col-md-6 col-sm-12 col-12">
@@ -57,7 +58,7 @@
                                     <span><i class="fa fa-fw fa-arrow-up"></i></span><span>5.86%</span>
                                 </div>
                             </div>
-                            <div id="sparkline-revenue2"></div>
+                            {{-- <div id="sparkline-revenue2"></div> --}}
                         </div>
                     </div>
                     <div class="col-xl-3 col-lg-6 col-md-6 col-sm-12 col-12">
@@ -71,7 +72,7 @@
                                     <span>N/A</span>
                                 </div>
                             </div>
-                            <div id="sparkline-revenue3"></div>
+                            {{-- <div id="sparkline-revenue3"></div> --}}
                         </div>
                     </div>
                     <div class="col-xl-3 col-lg-6 col-md-6 col-sm-12 col-12">
@@ -85,13 +86,25 @@
                                     <span>-2.00%</span>
                                 </div>
                             </div>
-                            <div id="sparkline-revenue4"></div>
+                            {{-- <div id="sparkline-revenue4"></div> --}}
                         </div>
                     </div>
                 </div>
                 <div class="row">
                     <!-- ============================================================== -->
-
+<div class="col-xl-7 col-lg-7 col-md-12 col-sm-12 col-12">
+                        <div class="card">
+                            <h5 class="card-header"> Total Revenue</h5>
+                            <div class="card-body">
+                                <div id="morris_totalrevenue"></div>
+                            </div>
+                            <div class="card-footer">
+                                <p class="display-7 font-weight-bold"><span
+                                        class="text-primary d-inline-block">$26,000</span><span
+                                        class="text-success float-right">+9.45%</span></p>
+                            </div>
+                        </div>
+                    </div>
                     <!-- ============================================================== -->
 
                     <!-- recent orders  -->
@@ -438,19 +451,7 @@
                     <!-- end category revenue  -->
                     <!-- ============================================================== -->
 
-                    <div class="col-xl-7 col-lg-7 col-md-12 col-sm-12 col-12">
-                        <div class="card">
-                            <h5 class="card-header"> Total Revenue</h5>
-                            <div class="card-body">
-                                <div id="morris_totalrevenue"></div>
-                            </div>
-                            <div class="card-footer">
-                                <p class="display-7 font-weight-bold"><span
-                                        class="text-primary d-inline-block">$26,000</span><span
-                                        class="text-success float-right">+9.45%</span></p>
-                            </div>
-                        </div>
-                    </div>
+                  
                 </div>
                 <div class="row">
                     <div class="col-xl-5 col-lg-6 col-md-6 col-sm-12 col-12">
