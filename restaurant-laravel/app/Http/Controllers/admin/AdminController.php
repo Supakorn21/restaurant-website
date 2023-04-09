e269c56db2efd891384218d3eb8eea9e2916dc29
Supakorn Charoen <supakorncharoen@gmail.com>
2023-04-09 15:42:07 +0700

    [03-laravel-back-end] - Total Revenue, customers, Reservations and Employees

--- restaurant-laravel/app/Http/Controllers/admin/AdminController.php
+++ restaurant-laravel/app/Http/Controllers/admin/AdminController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class AdminController extends Controller
 {
@@ -14,6 +15,42 @@ class AdminController extends Controller
 
     public function dashboard()
     {
-        return view('admin.dashboard');
+        $estimated_income_last_30 = DB::select(
+            DB::raw('
+            SELECT  (sum(guests_total)* 69) as total  FROM restaurant.reservations
+            WHERE created_at BETWEEN CURDATE()-INTERVAL 30 DAY AND CURDATE();
+        '),
+        )[0]->total;
+
+        $total_customers_last_30 = DB::select(
+            DB::raw('
+            SELECT  sum(guests_total) as total  FROM restaurant.reservations
+            WHERE created_at BETWEEN CURDATE()-INTERVAL 30 DAY AND CURDATE();
+        '),
+        )[0]->total;
+
+        $total_reservations_last_30 = DB::select(
+            DB::raw('
+            SELECT  count(*) as total  FROM restaurant.reservations
+            WHERE created_at BETWEEN CURDATE()-INTERVAL 30 DAY AND CURDATE();
+        '),
+        )[0]->total;
+
+        $total_employees = DB::select(
+            DB::raw('
+        SELECT count(*) as total FROM users 
+        inner join role_user 
+        on role_user.user_id = users.id
+        inner join roles on roles.id = role_user.role_id
+        where role_id = 2;
+        '),
+        )[0]->total;
+
+        return view('admin.dashboard', [
+            'estimated_income_last_30' => $estimated_income_last_30,
+            'total_customers_last_30' => $total_customers_last_30,
+            'total_reservations_last_30' => $total_reservations_last_30,
+            'total_employees' => $total_employees,
+        ]);
     }
 }
