ba9368c02c303884e627b7351bba7aaf2cecc202
Supakorn Charoen <supakorncharoen@gmail.com>
2023-04-28 16:31:36 +0700

    Fixed and Organize register page

--- restaurant-laravel/app/Models/User.php
+++ restaurant-laravel/app/Models/User.php
@@ -48,6 +48,7 @@ class User extends Authenticatable
     {
         return $this->belongsToMany(Role::class);
     }
+
     public function isAdmin()
     {
         if ($this->roles->contains(1)) {
--- restaurant-laravel/resources/views/auth/register.blade.php
+++ restaurant-laravel/resources/views/auth/register.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Concept - Bootstrap 4 Admin Dashboard Template</title>
-     <!-- CSRF Token -->
+    <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="/assets/vendor/bootstrap/css/bootstrap.min.css">
@@ -27,6 +27,7 @@
             padding-bottom: 40px;
         }
     </style>
+     
 </head>
 <!-- ============================================================== -->
 <!-- signup form  -->
@@ -37,85 +38,78 @@
     <!-- signup form  -->
     <!-- ============================================================== -->
     <form class="splash-container" method="POST" action="{{ route('register') }}">
-           @csrf
+        @csrf
         <div class="card">
             <div class="card-header">
                 <h3 class="mb-1">Registrations Form</h3>
                 <p>Please enter your user information.</p>
             </div>
             <div class="card-body">
-                   <div class="form-group ">
-                        <input id="fname" type="text"
-                            class="form-control form-control-lg @error('fname') is-invalid @enderror" name="fname"
-                            placeholder="First Name" value="{{ old('fname') }}" required autocomplete="name" autofocus>
-                        @error('fname')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                        @enderror
-                    </div>
-                 
-                    <div class="form-group ">
-                        <input id="lname" type="text"
-                            class="form-control form-control-lg @error('lname') is-invalid @enderror" name="lname"
-                            placeholder="Last Name" value="{{ old('lname') }}" required autocomplete="name" autofocus>
-                        @error('lname')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                        @enderror
-                    </div>
-                    <div class="form-group ">
-                        <input id="email" type="email"
-                            class="form-control form-control-lg @error('email') is-invalid @enderror" name="email"
-                            placeholder="Email" value="{{ old('email') }}" required autocomplete="email">
+                <div class="form-group ">
+                    <input id="fname" type="text"
+                        class="form-control form-control-lg @error('fname') is-invalid @enderror" name="fname"
+                        placeholder="First Name" value="{{ old('fname') }}" required autocomplete="name" autofocus>
+                    @error('fname')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
 
-                        @error('email')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                        @enderror
-                    </div>
-                    <div class="form-group ">
-                        <input id="password" type="password"
-                            class="form-control form-control-lg @error('password') is-invalid @enderror" name="password"
-                            required autocomplete="new-password" placeholder="Password">
+                <div class="form-group ">
+                    <input id="lname" type="text"
+                        class="form-control form-control-lg @error('lname') is-invalid @enderror" name="lname"
+                        placeholder="Last Name" value="{{ old('lname') }}" required autocomplete="name" autofocus>
+                    @error('lname')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
+                <div class="form-group ">
+                    <input id="email" type="email"
+                        class="form-control form-control-lg @error('email') is-invalid @enderror" name="email"
+                        placeholder="Email" value="{{ old('email') }}" required autocomplete="email">
 
-                        @error('password')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                        @enderror
-                    </div>
-                    <div class="form-group ">
-                        <input id="password-confirm" type="password" class="form-control form-control-lg"
-                            placeholder="Confirm password" name="password_confirmation" required
-                            autocomplete="new-password">
-                    </div>
+                    @error('email')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
+                <div class="form-group ">
+                    <input id="password" type="password"
+                        class="form-control form-control-lg @error('password') is-invalid @enderror" name="password"
+                        required autocomplete="new-password" placeholder="Password">
+
+                    @error('password')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
+                <div class="form-group ">
+                    <input id="password-confirm" type="password" class="form-control form-control-lg"
+                        placeholder="Confirm password" name="password_confirmation" required
+                        autocomplete="new-password">
+                </div>
+                <div class="form-group">
+                    <label class="custom-control custom-checkbox">
+                        <input class="custom-control-input" type="checkbox"><span class="custom-control-label">By
+                            creating an account, you agree the <a href="#">terms and conditions</a></span>
+                    </label>
+                </div>
+
+                <div class="form-group pt-2">
+                    <button class="btn btn-block btn-primary" type="submit"> {{ __('Register') }}</button>
+                </div>
 
 
-                    <div class="form-group pt-2">
-                        <button class="btn btn-block btn-primary" type="submit"> {{ __('Register') }}</button>
-                    </div>
-                    <div class="form-group">
-                        <label class="custom-control custom-checkbox">
-                            <input class="custom-control-input" type="checkbox"><span class="custom-control-label">By
-                                creating an account, you agree the <a href="#">terms and conditions</a></span>
-                        </label>
-                    </div>
-                    <div class="form-group row pt-0">
-                        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 mb-2">
-                            <button class="btn btn-block btn-social btn-facebook " type="button">Facebook</button>
-                        </div>
-                        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-                            <button class="btn  btn-block btn-social btn-twitter" type="button">Twitter</button>
-                        </div>
-                    </div>
             </div>
-    <div class="card-footer bg-white">
-        <p>Already member? <a href="/login" class="text-secondary">Login Here.</a></p>
-    </div>
-    </div>
+            <div class="card-footer bg-white">
+                <p>Already member? <a href="/login" class="text-secondary">Login Here.</a></p>
+            </div>
+        </div>
     </form>
 
 
